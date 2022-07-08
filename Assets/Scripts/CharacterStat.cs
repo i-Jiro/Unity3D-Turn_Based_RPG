f9c09e9bbe6af107f5a5d0fd60dee71b1445a538
i-Jiro <nguyen.andy.dev@gmail.com>
2022-07-07 23:59:03 -0400

    Stats modifiers order & additive percentage
    * Stat modifiers can be calculated at a given order.
    * Percentage stats modifiers can be multiplicate or additive

--- Assets/Scripts/CharacterStat.cs
+++ Assets/Scripts/CharacterStat.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 
-
 public class CharacterStat 
 {
     public float baseValue;
@@ -33,6 +32,7 @@ public class CharacterStat
     {
         _isDirty = true;
         _statModifiers.Add(mod);
+        _statModifiers.Sort(CompareModifierOrder);
     }
 
     public bool RemoveModifier(StatModifier mod)
@@ -41,13 +41,43 @@ public class CharacterStat
         return _statModifiers.Remove(mod);
     }
 
+    private int CompareModifierOrder(StatModifier a, StatModifier b)
+    {
+        if(a.order < b.order)
+        {
+            return -1;
+        }
+        else if(a.order > b.order)
+        {
+            return 1;
+        }
+
+        return 0;
+    }
 
     private float CaculateFinalValue()
     {
         float finalValue = baseValue;
+        float sumPercentAdd = 0;
         for(int i = 0; i < _statModifiers.Count; i++)
         {
-            finalValue += _statModifiers[i].value;
+            if(_statModifiers[i].type == StatModifierType.Flat)
+            {
+                finalValue += _statModifiers[i].value;
+            }
+            else if(_statModifiers[i].type == StatModifierType.PercentAdd)
+            {
+                sumPercentAdd += _statModifiers[i].value;
+                if(_statModifiers.Count < i + 1 || _statModifiers[i+1].type != StatModifierType.PercentAdd)
+                {
+                    finalValue *= 1 + sumPercentAdd;
+                    sumPercentAdd = 0;
+                }
+            }
+            else if(_statModifiers[i].type == StatModifierType.PercentMultiply)
+            {
+                finalValue *= 1 + _statModifiers[i].value;
+            }
         }
         _isDirty = false;
         return (float)Math.Round(finalValue, 4);
--- Assets/Scripts/StatModifier.cs
+++ Assets/Scripts/StatModifier.cs
@@ -2,11 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum StatModifierType { Flat, PercentAdd, PercentMultiply}
+
 public class StatModifier
 {
+    public readonly int order;
     public readonly float value;
-    public StatModifier(float value)
+    public readonly StatModifierType type;
+    public readonly object source;
+    public StatModifier(float value, StatModifierType type, int order, object source)
     {
         this.value = value;
+        this.order = order;
+        this.source = source;
     }
+
+    public StatModifier(float value, StatModifierType type) : this(value, type, (int)type, null) { }
+    public StatModifier(float value, StatModifierType type, int order) : this(value, type, order, null) { }
+    public StatModifier(float value, StatModifierType type, object source) : this(value, type, (int)type, source) { }
+
 }
