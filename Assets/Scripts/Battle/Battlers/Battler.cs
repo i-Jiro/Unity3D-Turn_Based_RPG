3a6f3c4e7242b63a1df3f81be6b413675c6a6638
i-Jiro <nguyen.andy.dev@gmail.com>
2022-07-10 01:14:36 -0400

    Base Battler class for inheritance
    A base class that will be inherited by hero characters and enemy character.
    Refactored enemy class so far with it.

---
+++ Assets/Scripts/Battle/Battlers/Battler.cs
@@ -0,0 +1,157 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+public abstract class Battler : MonoBehaviour
+{
+    protected const float CHANCE_CAP = 1000f;
+    protected const float TURN_TIMER_MAX = 100;
+
+    [SerializeField] protected string charName = "";
+    [SerializeField] protected float _currentHealth = 100;
+    [SerializeField] protected float _currentMana = 100;
+
+    [SerializeField] protected CharacterStat maxHealthStat;
+    [SerializeField] protected CharacterStat maxManaStat;
+    [SerializeField] protected CharacterStat physicalAttackStat;
+    [SerializeField] protected CharacterStat physicalDefenseStat;
+    [SerializeField] protected CharacterStat speedStat;
+    [SerializeField] protected CharacterStat criticalStat;
+    [SerializeField] protected CharacterStat evasionStat;
+
+    [SerializeField] protected float _baseDamageMultiplier = 1.0f;
+
+    [SerializeField]protected List<AbilityData> abilitiesData;
+    protected readonly List<Ability> abilities;
+    public readonly ReadOnlyCollection<Ability> Abilities;
+    protected Dictionary<StatusEffectData, StatusEffect> statusEffects;
+
+    protected float turnTimer = 0;
+    protected bool isTurnTimerActive = false;
+
+    public string Name
+    {
+        get { return charName; }
+        set { if (value.Length > 49) { Debug.LogWarning("Battler set with a name longer than 49 characters! Name will not fit UI."); } }
+    }
+
+    public float CurrentHealth { get { return _currentHealth; } }
+    public float CurrentMana { get { return _currentMana; } }
+    public float MaxHealth { get { return maxHealthStat.Value; } }
+    public float MaxMana { get { return maxManaStat.Value; } }
+
+    public Battler()
+    {
+        abilitiesData = new List<AbilityData>();
+        abilities = new List<Ability>();
+        Abilities = abilities.AsReadOnly();
+    }
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        _currentHealth = maxHealthStat.Value;
+        _currentMana = maxManaStat.Value;
+        isTurnTimerActive = true;
+        if (FindObjectOfType<BattleManager>())
+        {
+            BattleManager.OnActiveTurnChanged += ToggleTurnTimer;
+        }
+        else { Debug.LogError(gameObject.name + ": Can't find Battle Manager on scnene!"); }
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        if (isTurnTimerActive)
+            TickTurnTimer();
+    }
+
+    protected abstract void StartTurn();
+    protected abstract void EndTurn();
+
+    public virtual float CalculateDamage(float damageMultiplier)
+    {
+        float finalDamage = physicalAttackStat.Value * damageMultiplier;
+        float critChance = criticalStat.Value;
+        float randValue = Random.value;
+        if (critChance > CHANCE_CAP) { critChance = CHANCE_CAP; }
+        critChance /= CHANCE_CAP;
+        if (randValue < 1 - critChance)
+        {
+            float critMultiplier = 1.25f;
+            finalDamage *= critMultiplier;
+        }
+        return Mathf.Round(Random.Range(finalDamage, (finalDamage * 1.01f)));
+    }
+
+    protected virtual void TickTurnTimer()
+    {
+        if (turnTimer < TURN_TIMER_MAX)
+        {
+            turnTimer += Time.deltaTime * speedStat.Value;
+        }
+        else if (turnTimer > TURN_TIMER_MAX)
+        {
+            Debug.Log(gameObject.name + " has reached it's turn.");
+            StartTurn();
+        }
+    }
+
+    public virtual void ToggleTurnTimer(bool value)
+    {
+        isTurnTimerActive = !value;
+    }
+
+    public virtual void TakeDamage(float rawDamage)
+    {
+        float damage = rawDamage - physicalDefenseStat.Value;
+        if (damage < 0)
+            damage = 0;
+        _currentHealth -= damage;
+    }
+
+    protected void TickStatusEffects()
+    {
+        foreach (StatusEffect status in statusEffects.Values.ToList()) //creates copy into a list to iterate with. Avoids error if iterating and operating in oringal dict.
+        {
+            status.Tick();
+            if (status.isFinished)
+            {
+                statusEffects.Remove(status.Data);
+            }
+        }
+    }
+
+    public void AddStatusEffect(StatusEffect statusEffect)
+    {
+        Debug.Log("Status effect added: " + statusEffect.Data.Name + " on " + this.Name);
+        if (statusEffects.ContainsKey(statusEffect.Data))
+        {
+            statusEffects[statusEffect.Data].Start();
+        }
+        else
+        {
+            statusEffects.Add(statusEffect.Data, statusEffect);
+            statusEffects[statusEffect.Data].Start();
+        }
+    }
+
+    public void AddModifier(StatModifier statMod)
+    {
+        speedStat.AddModifier(statMod);
+    }
+
+    public void RemoveModifier(StatModifier statMod)
+    {
+        speedStat.RemoveModifier(statMod);
+    }
+
+    public void RemoveAllModifierFromSource(object source)
+    {
+        speedStat.RemoveAllModifierFromSource(source);
+    }
+
+}
--- Assets/Scripts/Battle/Battlers/Enemy.cs
+++ Assets/Scripts/Battle/Battlers/Enemy.cs
@@ -2,23 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : Battler
 {
-    [SerializeField] protected string charName = "";
-    public string Name
-    {
-        get { return charName; }
-        private set { if (value.Length > 8) { Debug.LogWarning("Enemy set with a name longer than 8 characters!"); } }
-    }
-    [SerializeField] protected float currentHealth = 100;
-    [SerializeField] protected float currentMana = 100;
-    [SerializeField] protected float defence = 10;
-    [SerializeField] protected float speed = 1;
-
-    protected float turnTimer = 0;
-    protected float turnTimerMax = 100;
-    protected bool isTurnTimerActive = false;
-
     protected List<Hero> heroes;
 
     public delegate void EventTakeTurn(Enemy enemy);
@@ -26,72 +11,45 @@ public class Enemy : MonoBehaviour
     public delegate void EventEndTurn();
     public event EventEndTurn OnEndTurn;
 
-    protected virtual void Start()
+    protected override void Start()
     {
-        isTurnTimerActive = true;
-        if (FindObjectOfType<BattleManager>())
-        {
-            BattleManager.OnActiveTurnChanged += ToggleTurnTimer;
-            heroes = BattleManager.Instance.heroes;
-        }
+        base.Start();
+        heroes = BattleManager.Instance.heroes;
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        if (isTurnTimerActive)
-            TickTurnTimer();
+        base.Update();
     }
 
-    public virtual void ToggleTurnTimer(bool value)
-    {
-        isTurnTimerActive = !value;
-    }
-
-    public virtual void Attack(Hero hero)
+    protected virtual void Attack(Hero hero)
     {
         Debug.Log(gameObject.name + " attacked " + hero.gameObject.name);
-        hero.TakeDamage(15);
+        hero.TakeDamage(CalculateDamage(_baseDamageMultiplier));
         EndTurn();
     }
 
-    public virtual void TakeDamage(float rawDamage)
-    {
-        currentHealth -= rawDamage;
-    }
-
-    public virtual Hero PickRandomHero()
+    protected virtual Hero PickRandomHero()
     {
         int index = Random.Range(0, heroes.Count);
         Hero hero = heroes[index];
         return hero;
     }
 
-    public virtual void TickTurnTimer()
-    {
-        if (turnTimer < turnTimerMax)
-        {
-            turnTimer += Time.deltaTime * speed;
-        }
-        else if (turnTimer > turnTimerMax)
-        {
-            Debug.Log(gameObject.name + " has reached it's turn.");
-            StartTurn();         
-        }
-    }
-
-    public virtual void StartTurn()
+    protected override void StartTurn()
     {
         OnTakeActiveTurn.Invoke(this);
         Attack(PickRandomHero());
     }
-    public virtual void EndTurn()
+
+    protected override void EndTurn()
     {
         turnTimer = 0;
         OnEndTurn.Invoke();
     }
 
-    private void OnDestroy()
+    protected void OnDestroy()
     {
         if (FindObjectOfType<BattleManager>())
         {
