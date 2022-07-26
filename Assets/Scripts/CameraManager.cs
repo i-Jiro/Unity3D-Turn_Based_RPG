978d5fc30ba17f2dcf3fbb1117ba0c2739a28145
i-Jiro <nguyen.andy.dev@gmail.com>
2022-07-26 10:51:09 -0400

    Frame shot of the battle scene
    *Frames the camera to have all the battlers in camera view using a target group.

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -3,16 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.Serialization;
 
-[RequireComponent(typeof(CinemachineTargetGroup))]
 public class CameraManager : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera _battleSceneCamera;
     [SerializeField] CinemachineVirtualCamera _focusHeroCamera;
-    [Header("Focus Group Targeting Settings")]
+    [Header("Focus Group On Targeting Settings")]
     [SerializeField] float _targetFocusWeight = 1.0f;
     [SerializeField] float _otherFocusWeight = 1.5f;
-    private CinemachineTargetGroup _targetGroup;
+    [SerializeField] private CinemachineTargetGroup _battleSceneTargetGroup;
+    [SerializeField] private CinemachineTargetGroup _focusTargetGroup;
     private bool _focusedOnGroup;
     private bool _isInitialized;
     
@@ -32,8 +33,8 @@ public class CameraManager : MonoBehaviour
     private void Start()
     {
         _focusHeroCamera.gameObject.SetActive(false);
-        _targetGroup = GetComponent<CinemachineTargetGroup>();
         SubscribeToEvents();
+        InitializeBattleSceneCam();
         _isInitialized = true;
     }
 
@@ -57,6 +58,21 @@ public class CameraManager : MonoBehaviour
         }
     }
 
+    private void InitializeBattleSceneCam()
+    {
+        if (_battleSceneTargetGroup == null) { Debug.LogWarning("BattleScene CM Target Group not assigned!"); return;}
+        foreach (var hero in BattleManager.Instance.heroes)
+        {
+            _battleSceneTargetGroup.AddMember(hero.transform,1,0);
+        }
+
+        foreach (var enemy in BattleManager.Instance.enemies)
+        {
+            _battleSceneTargetGroup.AddMember(enemy.transform, 1 ,0);
+        }
+        _battleSceneCamera.LookAt = _battleSceneTargetGroup.Transform;
+    }
+
     private void FocusOnHero(Battler battler)
     {
        _focusHeroCamera.gameObject.SetActive(true);
@@ -65,10 +81,11 @@ public class CameraManager : MonoBehaviour
 
     private void FocusOnGroup(Battler battler, Battler other)
     {
-        _targetGroup.AddMember(battler.transform, _targetFocusWeight, 0);
-        _targetGroup.AddMember(other.transform, _otherFocusWeight, 0);
+        if (_focusTargetGroup == null) { Debug.LogWarning("Focus CM Target Group not assigned!"); return;}
+        _focusTargetGroup.AddMember(battler.transform, _targetFocusWeight, 0);
+        _focusTargetGroup.AddMember(other.transform, _otherFocusWeight, 0);
         _focusHeroCamera.gameObject.SetActive(true);
-        _focusHeroCamera.LookAt = _targetGroup.transform;
+        _focusHeroCamera.LookAt = _focusTargetGroup.transform;
         _focusedOnGroup = true;
     }
 
@@ -76,9 +93,9 @@ public class CameraManager : MonoBehaviour
     {
         if (_focusedOnGroup)
         {
-            foreach (var target in _targetGroup.m_Targets)
+            foreach (var target in _focusTargetGroup.m_Targets)
             {
-                _targetGroup.RemoveMember(target.target);
+                _focusTargetGroup.RemoveMember(target.target);
             }
         }
         _focusHeroCamera.LookAt = null;
